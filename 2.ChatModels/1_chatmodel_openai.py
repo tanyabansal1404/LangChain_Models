967bdc512730fbda6e63675270eb435bbe22c68b
tanyabansal1404 <tanyabansal1404@gmail.com>
2025-12-10 10:54:25 +0530

    Working with Closed-Source Chat Models

---
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -0,0 +1,12 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model='gpt-4', temperature=0, max_completion_tokens=10)
+
+result = model.invoke("Write a 5 line poem on cricket")
+
+# print(result)
+
+print(result.content)
